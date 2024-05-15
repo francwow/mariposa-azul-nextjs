0eba6830187087719ccf986853451a9a158de8be
Francisco Osorio <francwow06@gmail.com>
2024-05-15 11:22:13 -0500

    add await keyword to fetch

--- src/app/components/InstagramFeed.tsx
+++ src/app/components/InstagramFeed.tsx
@@ -1,23 +1,22 @@
 import Image from "next/image";
 import Link from "next/link";
-import { getData } from "../api/route";
 
-// async function getData() {
-//   const res = await fetch(
-//     `https://graph.instagram.com/me/media?fields=id,media_url&access_token=${process.env.INSTAGRAM_KEY}`
-//   );
-//   // The return value is *not* serialized
-//   // You can return Date, Map, Set, etc.
+async function getData() {
+  const res = await fetch(
+    `https://graph.instagram.com/me/media?fields=id,media_url&access_token=${process.env.INSTAGRAM_KEY}`
+  );
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
 
-//   if (!res.ok) {
-//     // This will activate the closest `error.js` Error Boundary
-//     throw new Error("Failed to fetch data");
-//   }
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
 
-//   const feed = res.json();
+  const feed = await res.json();
 
-//   return feed;
-// }
+  return feed;
+}
 
 const InstagramFeed = async () => {
   const feed = await getData();
