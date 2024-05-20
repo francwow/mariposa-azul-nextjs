fdcae4574bf9b94a562a3289496cc6d09bf56e64
Francisco Osorio <francwow06@gmail.com>
2024-05-20 17:09:20 -0500

    fix fetch 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,31 @@
 import Footer from "./components/Footer";
 import Inicio from "./components/Inicio";
-// import Link from "next/link";
-// import Image from "next/image";
+import InstagramFeed from "./components/InstagramFeedCopy";
 
-// async function getData() {
-//   let url = `https://graph.instagram.com/me/media?fields=id,media_url&access_token=${process.env.INSTAGRAM_KEY}`;
-//   const res = await fetch(url);
-//   // The return value is *not* serialized
-//   // You can return Date, Map, Set, etc.
+async function getData() {
+  let url = `https://graph.instagram.com/me/media?fields=id,media_url&access_token=${process.env.INSTAGRAM_KEY}`;
+  const res = await fetch(url);
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
 
-//   if (!res.ok) {
-//     // This will activate the closest `error.js` Error Boundary
-//     throw new Error("Failed to fetch data");
-//   }
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
 
-//   return res.json();
-// }
+  return res.json();
+}
 
-export default function Home() {
+export default async function Home() {
+  const feed = await getData();
+  const images = feed.data;
+  console.log(images);
+
   return (
     <main className="main-container">
       <Inicio />
-      {/* <InstagramFeed /> */}
+      <InstagramFeed images={images} />
       <Footer />
     </main>
   );
 }
-
-// const InstagramFeed = async () => {
-//   const feed = await getData();
-//   const images = feed.data;
-//   console.log(images);
-
-//   return (
-//     <section className="instagram-feed-container">
-//       <div className="instagram-feed">
-//         {images !== null
-//           ? images.map((image: any) => {
-//               return (
-//                 <Link
-//                   href={"https://www.instagram.com/somos.mariposa.azul/"}
-//                   target="_blank"
-//                   className="instagram-item"
-//                   key={image.id}
-//                 >
-//                   <Image
-//                     src={image.media_url}
-//                     alt="instagram image"
-//                     width={600}
-//                     height={600}
-//                   />
-//                 </Link>
-//               );
-//             })
-//           : null}
-//       </div>
-//     </section>
-//   );
-// };
