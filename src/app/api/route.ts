f4b3783c1d614b791100d32c8b8bfa97ebfd7d97
Francisco Osorio <francwow06@gmail.com>
2024-05-20 09:59:21 -0500

    instagram client test6

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 export async function GET() {
   const response = await fetch(
     `https://graph.instagram.com/me/media?fields=id,media_url&access_token=${process.env.INSTAGRAM_KEY}`
