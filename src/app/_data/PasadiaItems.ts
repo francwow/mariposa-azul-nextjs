97070c4391c7a31cf0ff85a84b44dd026c90afd6
Francisco Osorio <francwow06@gmail.com>
2024-07-02 18:38:24 -0500

    add styles to Pasadia section

---
+++ src/app/_data/PasadiaItems.ts
@@ -0,0 +1,64 @@
+import { StaticImageData } from "next/image";
+import img1 from "../../../public/pasadia/pasadia1.webp";
+import img2 from "../../../public/pasadia/pasadia3.webp";
+import img3 from "../../../public/pasadia/pasadia4.webp";
+import img4 from "../../../public/pasadia/pasadia5.webp";
+import img5 from "../../../public/pasadia/pasadia6.webp";
+import img6 from "../../../public/pasadia/pasadia7.webp";
+import img7 from "../../../public/pasadia/pasadia8.webp";
+import img8 from "../../../public/pasadia/pasadia9.webp";
+import img9 from "../../../public/pasadia/pasadia10.webp";
+
+type PasadiaItem = {
+  imgSrc: StaticImageData;
+  altES: string;
+  altEN: string;
+};
+
+export const PasadiaItems: PasadiaItem[] = [
+  {
+    imgSrc: img1,
+    altES: "Imagen de Retiro ofrecido por Mariposa Azul",
+    altEN: "Picture from a retreat with Mariposa Azul",
+  },
+  {
+    imgSrc: img2,
+    altES: "Imagen de Retiro ofrecido por Mariposa Azul",
+    altEN: "Picture from a retreat with Mariposa Azul",
+  },
+  {
+    imgSrc: img3,
+    altES: "Imagen de Retiro ofrecido por Mariposa Azul",
+    altEN: "Picture from a retreat with Mariposa Azul",
+  },
+  {
+    imgSrc: img4,
+    altES: "Imagen de Retiro ofrecido por Mariposa Azul",
+    altEN: "Picture from a retreat with Mariposa Azul",
+  },
+  {
+    imgSrc: img5,
+    altES: "Imagen de Retiro ofrecido por Mariposa Azul",
+    altEN: "Picture from a retreat with Mariposa Azul",
+  },
+  {
+    imgSrc: img6,
+    altES: "Imagen de Retiro ofrecido por Mariposa Azul",
+    altEN: "Picture from a retreat with Mariposa Azul",
+  },
+  {
+    imgSrc: img7,
+    altES: "Imagen de Retiro ofrecido por Mariposa Azul",
+    altEN: "Picture from a retreat with Mariposa Azul",
+  },
+  {
+    imgSrc: img8,
+    altES: "Imagen de Retiro ofrecido por Mariposa Azul",
+    altEN: "Picture from a retreat with Mariposa Azul",
+  },
+  {
+    imgSrc: img9,
+    altES: "Imagen de Retiro ofrecido por Mariposa Azul",
+    altEN: "Picture from a retreat with Mariposa Azul",
+  },
+];
--- src/app/components/PasadiaInfo.tsx
+++ src/app/components/PasadiaInfo.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import Image from "next/image";
 import { useLanguage } from "../_contexts/ContextHooks";
+import { PasadiaItems } from "../_data/PasadiaItems";
 import InViewEl from "./InView";
 import MainBtn from "./MainBtn";
 
@@ -8,85 +10,125 @@ const PasadiaInfo = () => {
   const { language } = useLanguage();
 
   return (
-    <section className="somos-info-section content-section">
-      <div className="info-text">
-        {/* <InViewEl
-          activeClass="info-heading fade-up"
-          notActiveClass="info-heading"
-        >
-          <h2>{language === "ES" ? "Sobre Mariposa" : "About Mariposa"}</h2>
-        </InViewEl> */}
+    <section className="pasadia-info-section content-section">
+      <div className="info-container">
         {language === "ES" ? (
-          <InViewEl activeClass="info fade-up" notActiveClass="info">
-            <p>
-              Durante un día tendrás la oportunidad de vivir la “Experiencia
-              Mariposa Azul” de forma individual, en pareja o en grupo. Esta
-              experiencia incluye:
-            </p>
-            <br />
-            <br />
-            <ul>
-              <li>Circulo de apertura e intención y Oráculo</li>
-              <li>
-                Reconocimiento del territorio e inmersión en río / cascada
-              </li>
-              <li>
-                Sesión de meditación con geometría sagrada + Sound healing con
-                cuencos
-              </li>
-              <li>Almuerzo Saludable</li>
-            </ul>
-            <br />
-            <br />
-            <p>
-              Si deseas incluir alguna terapia alternativa cuéntanos al momento
-              de contactarnos
-            </p>
-            <br />
-            <br />
-            <MainBtn
-              href="https://wa.me/573104823549?text=¡Hola%20Mariposa%20Azul!%20Estoy%20interesad@%20en%20..."
-              target
-              bgColor="green"
-            >
-              Más información aquí
-            </MainBtn>
-          </InViewEl>
+          <div className="info-wrapper">
+            <div className="pasadia-info">
+              <InViewEl activeClass="info fade-up" notActiveClass="info">
+                <div className="info-text">
+                  <h3>
+                    Durante un día tendrás la oportunidad de vivir la
+                    “Experiencia Mariposa Azul” de forma individual, en pareja o
+                    en grupo. Esta experiencia incluye:
+                  </h3>
+                  <ul className="pasadia-items">
+                    <li>Circulo de apertura e intención y Oráculo</li>
+                    <li>
+                      Reconocimiento del territorio e inmersión en río / cascada
+                    </li>
+                    <li>
+                      Sesión de meditación con geometría sagrada + Sound healing
+                      con cuencos
+                    </li>
+                    <li>Almuerzo Saludable</li>
+                  </ul>
+                </div>
+              </InViewEl>
+              <InViewEl activeClass="info fade-up" notActiveClass="info">
+                <div className="pasadia-images">
+                  {PasadiaItems.map((item, index) => (
+                    <div key={index} className="pasadia-img">
+                      <Image
+                        src={item.imgSrc.src}
+                        alt={item.altES}
+                        width={500}
+                        height={500}
+                      />
+                    </div>
+                  ))}
+                </div>
+              </InViewEl>
+            </div>
+            <div className="pasadia-cta">
+              <InViewEl activeClass="info fade-up" notActiveClass="info">
+                <div className="info-text">
+                  <h3>
+                    Si deseas incluir alguna terapia alternativa cuéntanos al
+                    momento de contactarnos.
+                  </h3>
+                </div>
+              </InViewEl>
+              <InViewEl activeClass="info fade-up" notActiveClass="info">
+                <MainBtn
+                  href="https://wa.me/573104823549?text=¡Hola%20Mariposa%20Azul!%20Estoy%20interesad@%20en%20..."
+                  target
+                  bgColor="green"
+                >
+                  Más información aquí
+                </MainBtn>
+              </InViewEl>
+            </div>
+          </div>
         ) : (
-          <InViewEl activeClass="info fade-up" notActiveClass="info">
-            <p>
-              For one day you will have the opportunity to live the “Experience
-              Blue Butterfly” individually, as a couple or in a group. This
-              experience includes:
-            </p>
-            <br />
-            <br />
-            <ul>
-              <li>Circle of opening and intention and Oracle</li>
-              <li>
-                Recognition of the territory and immersion in river / waterfall
-              </li>
-              <li>
-                Meditation session with sacred geometry + Sound healing with
-                bowls
-              </li>
-              <li>Healthy lunch</li>
-            </ul>
-            <br />
-            <br />
-            <p>
-              If you want to include any alternative therapy, please let us know
-            </p>
-            <br />
-            <br />
-            <MainBtn
-              href="https://wa.me/573104823549?text=¡Hola%20Mariposa%20Azul!%20Estoy%20interesad@%20en%20..."
-              target
-              bgColor="green"
-            >
-              More info here
-            </MainBtn>
-          </InViewEl>
+          <div className="info-wrapper">
+            <div className="pasadia-info">
+              <InViewEl activeClass="info fade-up" notActiveClass="info">
+                <div className="info-text">
+                  <h3>
+                    For one day you will have the opportunity to live the
+                    “Experience Blue Butterfly” individually, as a couple or in
+                    a group. This experience includes:
+                  </h3>
+                  <ul className="pasadia-items">
+                    <li>Circle of opening and intention and Oracle</li>
+                    <li>
+                      Recognition of the territory and immersion in river /
+                      waterfall
+                    </li>
+                    <li>
+                      Meditation session with sacred geometry + Sound healing
+                      with bowls
+                    </li>
+                    <li>Healthy lunch</li>
+                  </ul>
+                </div>
+              </InViewEl>
+              <InViewEl activeClass="info fade-up" notActiveClass="info">
+                <div className="pasadia-images">
+                  {PasadiaItems.map((item, index) => (
+                    <div key={index} className="pasadia-img">
+                      <Image
+                        src={item.imgSrc.src}
+                        alt={item.altES}
+                        width={500}
+                        height={500}
+                      />
+                    </div>
+                  ))}
+                </div>
+              </InViewEl>
+            </div>
+            <div className="pasadia-cta">
+              <InViewEl activeClass="info fade-up" notActiveClass="info">
+                <div className="info-text">
+                  <h3>
+                    If you want to include any alternative therapy, please let
+                    us know.
+                  </h3>
+                </div>
+              </InViewEl>
+              <InViewEl activeClass="info fade-up" notActiveClass="info">
+                <MainBtn
+                  href="https://wa.me/573104823549?text=¡Hola%20Mariposa%20Azul!%20Estoy%20interesad@%20en%20..."
+                  target
+                  bgColor="green"
+                >
+                  More info here
+                </MainBtn>
+              </InViewEl>
+            </div>
+          </div>
         )}
       </div>
     </section>
